070f737f299776228904da9fe9b8cf23acb554a3
kenthellck <praveengunaratna@yahoo.com>
2016-04-04 13:42:21 +0530

    user registration is complete

--- app/src/main/java/com/example/praveen/findone/Register.java
+++ app/src/main/java/com/example/praveen/findone/Register.java
@@ -3,7 +3,31 @@ package com.example.praveen.findone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.StringTokenizer;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 public class Register extends AppCompatActivity {
 
     @Override
@@ -11,5 +35,149 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         final Button btnReg = (Button) findViewById(R.id.regbutton);
+        btnReg.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // WebServer Request URL
+                String serverURL = "http://hoola2.bugs3.com/UserRegister.php";
+
+                // Use AsyncTask execute Method To Prevent ANR Problem
+                new LongOperation().execute(serverURL);
+            }
+        });
+    }
+
+    private class LongOperation  extends AsyncTask<String, Void, Void> {
+
+        // Required initialization
+
+        private final HttpClient Client = new DefaultHttpClient();
+        private String Content;
+        private String Error = null;
+        private ProgressDialog Dialog = new ProgressDialog(Register.this);
+        String email ="";
+        String password ="";
+        String age ="";
+        String name ="";
+        TextView uiUpdate = (TextView) findViewById(R.id.output);
+        TextView jsonParsed = (TextView) findViewById(R.id.jsonParsed);
+        int sizeData = 0;
+        EditText ename = (EditText) findViewById(R.id.regname);
+        EditText egae=(EditText)findViewById(R.id.regage);
+        EditText epass=(EditText)findViewById(R.id.regpassword);
+        EditText eemail=(EditText)findViewById(R.id.regemail);
+
+
+
+        protected void onPreExecute() {
+            // NOTE: You can call UI Element here.
+
+            //Start Progress Dialog (Message)
+
+            Dialog.setMessage("Please wait..");
+            Dialog.show();
+
+            try{
+                // Set Request parameter
+                email +="&" + URLEncoder.encode("data", "UTF-8") + "="+eemail.getText();
+                password=epass.getText().toString();
+                name=ename.getText().toString();
+                age=egae.getText().toString();
+            } catch (UnsupportedEncodingException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+        // Call after onPreExecute method
+        protected Void doInBackground(String... urls) {
+
+            /************ Make Post Call To Web Server ***********/
+            BufferedReader reader=null;
+
+            // Send data
+            try
+            {
+
+                // Defined URL  where to send data
+                URL url = new URL(urls[0]);
+                //  URL url2 = new URL(urls[1]);
+
+                // Send POST data request
+
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+                wr.write( email +'#');
+                wr.write( password+'#' );
+                wr.write( name+'#' );
+                wr.write(age);
+                wr.flush();
+
+
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+
+                // Read Server Response
+
+                sb.append(reader.readLine()+" ");
+
+                // Append Server Response To Content String
+                Content = sb.toString();
+                StringTokenizer token=new StringTokenizer(Content,"<");
+                while (token.hasMoreTokens())
+                {
+                    Content=token.nextToken();
+                        token.nextToken();
+                }
+            }
+            catch(Exception ex)
+            {
+                Error = ex.getMessage();
+            }
+            finally
+            {
+                try
+                {
+
+                    reader.close();
+                }
+
+                catch(Exception ex) {}
+            }
+
+            /*****************************************************/
+            return null;
+        }
+
+        protected void onPostExecute(Void unused) {
+            // NOTE: You can call UI Element here.
+
+            // Close progress dialog
+            Dialog.dismiss();
+
+            if (Error != null) {
+
+                uiUpdate.setText("Output : "+Error);
+
+            }
+            else {
+
+                // Show Response Json On Screen (activity)
+                if (Content.equals("done"))
+                {
+
+                    uiUpdate.setText( "good" );
+
+                }
+                uiUpdate.setText( Content );
+
+
+            }
+        }
+
     }
 }
